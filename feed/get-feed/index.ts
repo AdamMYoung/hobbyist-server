38c899ab96106b8d3272e40ca5678bcbf6746fe9
Adam Young <adamyoungg97@gmail.com>
2020-12-15 16:25:30 +0000

    Updated packages required

--- feed/get-feed/index.ts
+++ feed/get-feed/index.ts
@@ -1,15 +1,15 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import { withAuth } from "../utils/authUtils";
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import { withAuth } from '../utils/authUtils';
 
-const httpTrigger: AzureFunction = withAuth([], null, async function (context: Context, req: HttpRequest, token): Promise<void> {
-    
-    
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: "responseMessage"
-    };
-
-}};
+const httpTrigger: AzureFunction = withAuth(
+    [],
+    null,
+    async function (context: Context, req: HttpRequest, token): Promise<void> {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: 'responseMessage',
+        };
+    }
+);
 
 export default httpTrigger;
