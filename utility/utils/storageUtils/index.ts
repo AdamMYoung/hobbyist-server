e14ae71ede632484533261663baca50d0bd0219e
Adam Young <adamyoungg97@gmail.com>
2020-12-17 23:12:44 +0000

    Added minification to image upload

--- utility/utils/storageUtils/index.ts
+++ utility/utils/storageUtils/index.ts
@@ -1,5 +1,8 @@
 import { BlobServiceClient } from '@azure/storage-blob';
 import base64Img from 'base64-img';
+import imagemin from 'imagemin';
+import imageminJpegtran from 'imagemin-jpegtran';
+import imageminPngquant from 'imagemin-pngquant';
 
 import { ImageUpload } from '../../types';
 
@@ -13,9 +16,18 @@ export const uploadImage = async (upload: ImageUpload): Promise<string> => {
     const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 
     const image = await new Promise<string>((resolve) => {
-        base64Img.img(upload.base64Image, '', blobName, (err, filepath) => resolve(filepath));
+        base64Img.img(upload.base64Image, '', blobName, (_, filepath) => resolve(filepath));
     });
 
-    await blockBlobClient.uploadFile(image);
+    const minifiedImage = await imagemin([image], {
+        plugins: [
+            imageminJpegtran(),
+            imageminPngquant({
+                quality: [0.6, 0.7],
+            }),
+        ],
+    });
+
+    await blockBlobClient.uploadData(minifiedImage[0].data);
     return `${process.env.BLOB_STORAGE_ACCOUNT}.blob.core.windows.net/${upload.storageLocation}/${blobName}`;
 };
