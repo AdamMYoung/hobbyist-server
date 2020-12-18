2400824b2e9300b434c3c4a4da2b0a68237787ea
Adam Young <adamyoungg97@gmail.com>
2020-12-18 23:33:17 +0000

    Fixes to model validation

--- utility/utils/modelValidationUtils/index.ts
+++ utility/utils/modelValidationUtils/index.ts
@@ -1,5 +1,5 @@
 import { ImageUpload } from '../../types';
 
 export function isImageUpload(object: any): object is ImageUpload {
-    return 'base64Img' in object && 'storageLocation' in object;
+    return 'base64Image' in object && 'storageLocation' in object;
 }
