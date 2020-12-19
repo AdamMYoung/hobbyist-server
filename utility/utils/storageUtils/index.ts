164c1c8b8cafa0384c88e6a30001694a6777132c
Adam Young <adamyoungg97@gmail.com>
2020-12-19 20:04:37 +0000

    Removed image minification for testing

--- utility/utils/storageUtils/index.ts
+++ utility/utils/storageUtils/index.ts
@@ -21,15 +21,15 @@ export const uploadImage = async (upload: ImageUpload, context: Context): Promis
     const blobName = `img-${encodeURIComponent(new Date().toISOString())}.${fileType.ext}`;
     const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 
-    const minifiedImage = await imagemin.buffer(imageBuffer, {
-        plugins: [
-            imageminJpegtran(),
-            imageminPngquant({
-                quality: [0.6, 0.7],
-            }),
-        ],
-    });
+    // const minifiedImage = await imagemin.buffer(imageBuffer, {
+    //     plugins: [
+    //         imageminJpegtran(),
+    //         imageminPngquant({
+    //             quality: [0.6, 0.7],
+    //         }),
+    //     ],
+    // });
 
-    await blockBlobClient.uploadData(minifiedImage);
+    await blockBlobClient.uploadData(imageBuffer);
     return `https://${process.env.BLOB_STORAGE_ACCOUNT}.blob.core.windows.net/${upload.storageLocation}/${blobName}`;
 };
