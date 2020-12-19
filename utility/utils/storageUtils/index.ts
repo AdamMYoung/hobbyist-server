7d632416485512e7edbea0b1161070024d07aadb
Adam Young <adamyoungg97@gmail.com>
2020-12-19 20:53:51 +0000

    Moved to minification

--- utility/utils/storageUtils/index.ts
+++ utility/utils/storageUtils/index.ts
@@ -23,15 +23,15 @@ export const uploadImage = async (upload: ImageUpload, context: Context): Promis
     const blobName = `img-${getId()}.${fileType.ext}`;
     const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 
-    // const minifiedImage = await imagemin.buffer(imageBuffer, {
-    //     plugins: [
-    //         imageminJpegtran(),
-    //         imageminPngquant({
-    //             quality: [0.6, 0.7],
-    //         }),
-    //     ],
-    // });
+    const minifiedImage = await imagemin.buffer(imageBuffer, {
+        plugins: [
+            imageminJpegtran(),
+            imageminPngquant({
+                quality: [0.6, 0.7],
+            }),
+        ],
+    });
 
-    await blockBlobClient.uploadData(imageBuffer);
+    await blockBlobClient.uploadData(minifiedImage);
     return `https://${process.env.BLOB_STORAGE_ACCOUNT}.blob.core.windows.net/${upload.storageLocation}/${blobName}`;
 };
